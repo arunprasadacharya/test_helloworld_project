df1029c7a1c7bae3dae5c5d853711602801961e7
arunprasadacharya <arunprasadacharya@gmail.com>
2020-04-07 15:41:33 -0500

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello world ! from github.com..");
-        System.out.print("Sandesh Lamsal");
+        System.out.println("Hello World ! from github.com..");
+        System.out.print("Arun Prasad Acharya");
 	
     }
 }
